2667a0a5531c175bc26f98476f99e3e6bda85e65
Yubraj Pokharel <yubrajpokharel@HeartBleed.local>
2017-08-01 20:17:39 -0500

    person services

---
+++ src/main/java/com/yubraj/services/PersonService.java
@@ -0,0 +1,27 @@
+package com.yubraj.services;
+
+import com.yubraj.domain.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yubrajpokharel on 8/1/17.
+ */
+public class PersonService {
+
+    List<Person> personList = new ArrayList<Person>();
+
+    public void addPerson(Person person){
+        personList.add(person);
+    }
+
+    public void removePerson(Person person){
+        personList.remove(person);
+    }
+
+    public List<Person> getAllPerson(){
+        return personList;
+    }
+
+}
